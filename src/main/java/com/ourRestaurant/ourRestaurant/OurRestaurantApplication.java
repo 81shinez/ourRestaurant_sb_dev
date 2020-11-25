4292d58bae27fbac0a7a4c84bb34747a97aba26d
81Shinez <despiguile@gmail.com>
2020-11-25 22:33:55 +0900

    INITIAL COMMIT

---
+++ src/main/java/com/ourRestaurant/ourRestaurant/OurRestaurantApplication.java
@@ -0,0 +1,12 @@
+package com.ourRestaurant.ourRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurRestaurantApplication {
+
+	public static void main(String[] args) {SpringApplication.run(OurRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ourRestaurant/ourRestaurant/OurRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourRestaurant.ourRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
